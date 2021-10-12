4b6c4820487fb0a1d2588b22cfaa4ed00446a38b
yogkri <dharmayog91@gmail.com>
2021-10-12 08:27:13 +0530

    Welcome Message

---
+++ src/main/java/com/Bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Hotel Reservation System");
+    }
+}
