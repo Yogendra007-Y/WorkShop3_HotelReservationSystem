093dde389a573e876a1e868ee38b77d96733ab0d
yogkri <dharmayog91@gmail.com>
2021-10-12 10:01:02 +0530

    Ability to add hotels

---
+++ src/main/java/com/Bridgelabz/HotelReservationSystem.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HotelReservationSystem {
+    List<Hotel> hotels = new ArrayList<>();
+    /**
+     * This method is used to add hotel to the Hotel List
+     */
+    public boolean addHotel(Hotel hotel) {
+        return hotels.add(hotel);
+    }
+}
